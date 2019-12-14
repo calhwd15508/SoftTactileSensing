35c76a6113bc6d2a15aadaf3bfaf4b37ba7c7622
Class Account <ee106a-agh@c111-3.local>
2019-12-14 00:10:25 -0800

    deleted everything

--- build/lab4_cam/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/cc/ee106a/fa19/class/ee106a-agh/ros_workspaces/project/src/lab4_cam
-# Build directory: /home/cc/ee106a/fa19/class/ee106a-agh/ros_workspaces/project/build/lab4_cam
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/lab4_cam/catkin_generated/lab4_cam-msg-extras.cmake.develspace.in
+++
@@ -1,2 +0,0 @@
-set(lab4_cam_MESSAGE_FILES "")
-set(lab4_cam_SERVICE_FILES "/home/cc/ee106a/fa19/class/ee106a-agh/ros_workspaces/project/src/lab4_cam/srv/ImageSrv.srv")
